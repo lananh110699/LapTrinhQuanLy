428d4cb3dec900be8eb6a232ad3141293796d4ec
NGUYEN THI LAN ANH <1721050613@student.humg.edu.vn>
2022-03-01 09:09:28 +0700

    1.3

---
+++ NTLABaiThucHanh613/NTLABaiThucHanh613/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NTLABaiThucHanh613.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(String Masinhvien, string Tensinhvien)
+        {
+            ViewBag.Message = Masinhvien + "   " + Tensinhvien;
+            return View();
+        }
+    }
+}
