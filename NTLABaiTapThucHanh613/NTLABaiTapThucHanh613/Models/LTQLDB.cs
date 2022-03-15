0d6f4e257bf5f5276e0fb3f5bbd76637233bac68
anhntl <anhntl@runsystem.net>
2022-03-15 23:46:22 +0700

    9.3

---
+++ NTLABaiTapThucHanh613/NTLABaiTapThucHanh613/Models/LTQLDB.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace NTLABaiTapThucHanh613.Models
+{
+    public partial class LTQLDB : DbContext
+    {
+        public LTQLDB()
+            : base("name=LTQLDB")
+        {
+        }
+        public virtual DbSet<HangHoa> HangHoas { get; set; }
+        public virtual DbSet<DangNhap> DangNhaps { get; set; }
+        public virtual DbSet<XuatKho> XuatKhos { get; set; }
+        public virtual DbSet<NhapKho> NhapKhos { get; set; }
+        public virtual DbSet<NCC> NCCs { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
