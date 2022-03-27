a85740fdeece042e69c8d68213d4ba8760528630
anhntl <anhntl@runsystem.net>
2022-03-27 23:48:11 +0700

    23.3

---
+++ NTLABaiTapThucHanh613/NTLABaiTapThucHanh613/Controllers/HangHoasController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NTLABaiTapThucHanh613.Models;
+
+namespace NTLABaiTapThucHanh613.Controllers
+{
+    public class HangHoasController : Controller
+    {
+        private LTQLDB db = new LTQLDB();
+        AutoGenerateKey Aukey = new AutoGenerateKey();
+
+        // GET: HangHoas
+        public ActionResult Index()
+        {
+            return View(db.HangHoas.ToList());
+        }
+
+        // GET: HangHoas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HangHoa hangHoa = db.HangHoas.Find(id);
+            if (hangHoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hangHoa);
+        }
+
+        // GET: HangHoas/Create
+        public ActionResult Create()
+        {
+
+            if (db.HangHoas.OrderByDescending(m => m.MaHang).Count() == 0)
+            {
+                var newID = "MHH001";
+                ViewBag.NewMHHID = newID;
+            }
+            else
+            {
+                var MHHID = db.HangHoas.OrderByDescending(m => m.MaHang).FirstOrDefault().MaHang;
+                var newID = Aukey.GenerateKey(MHHID);
+                ViewBag.NewMHHID = newID;
+            }
+            return View();
+
+        }
+
+        // POST: HangHoas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaHang,TenHang,SoLuong,DonGia")] HangHoa hangHoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HangHoas.Add(hangHoa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(hangHoa);
+        }
+
+        // GET: HangHoas/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HangHoa hangHoa = db.HangHoas.Find(id);
+            if (hangHoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hangHoa);
+        }
+
+        // POST: HangHoas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaHang,TenHang,SoLuong,DonGia")] HangHoa hangHoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hangHoa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hangHoa);
+        }
+
+        // GET: HangHoas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HangHoa hangHoa = db.HangHoas.Find(id);
+            if (hangHoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hangHoa);
+        }
+
+        // POST: HangHoas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            HangHoa hangHoa = db.HangHoas.Find(id);
+            db.HangHoas.Remove(hangHoa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
